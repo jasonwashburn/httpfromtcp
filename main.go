96109564464c5ee2a4dc98917fc87b7a15071850
Jason Washburn <jason.washburn@gmail.com>
2025-08-05 06:52:53 -0500

    refactor: moved file read to go routine and channel

--- main.go
+++ main.go
@@ -9,36 +9,51 @@ import (
 )
 
 func main() {
-	file, err := os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		os.Exit(1)
 	}
 
+	ch := getLinesChannel(f)
+	for line := range ch {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
 	currentLineContents := ""
 
-	for {
-		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
+	go func() {
+		defer f.Close()
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err != nil {
+				if currentLineContents != "" {
+					ch <- currentLineContents
+					currentLineContents = ""
+				}
 
-			if errors.Is(err, io.EOF) {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Println("Error reading file:", err)
 				break
 			}
-			fmt.Println("Error reading file:", err)
-			break
-		}
 
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for _, part := range parts[:len(parts)-1] {
-			fmt.Printf("read: %s%s\n", currentLineContents, part)
-			currentLineContents = ""
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for _, part := range parts[:len(parts)-1] {
+				ch <- fmt.Sprintf("%s%s", currentLineContents, part)
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
 		}
-		currentLineContents += parts[len(parts)-1]
-	}
+		close(ch)
+	}()
+
+	return ch
 }
