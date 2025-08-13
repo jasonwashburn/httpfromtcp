25bf79ff1bc5e39ae3e9535810c9ea3cb690b808
Jason Washburn <jason.washburn@gmail.com>
2025-08-13 06:12:33 -0500

    feat: wip - add chunked reading

--- internal/request/request.go
+++ internal/request/request.go
@@ -16,6 +16,13 @@ type RequestLine struct {
 	Method        string
 }
 
+type ParserState int
+
+const (
+	ParserInitialized ParserState = iota
+	ParserDone
+)
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	r, err := io.ReadAll(reader)
 	if err != nil {
@@ -23,27 +30,34 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	}
 
 	lines := strings.Split(string(r), "\r\n")
-	requestLine, err := parseRequestLine(lines[0])
+	_, requestLine, err := parseRequestLine(lines[0])
 	return &Request{RequestLine: *requestLine}, err
 }
 
-func parseRequestLine(line string) (*RequestLine, error) {
+func parseRequestLine(line string) (int, *RequestLine, error) {
+	if !strings.Contains(line, "\r\n") {
+		// needs more data before we can parse the request line
+		return 0, &RequestLine{}, nil
+	}
+
 	parts := strings.Split(line, " ")
 	if len(parts) != 3 {
-		return &RequestLine{}, fmt.Errorf("rquest line must have exactly 3 parts")
+		return -1, &RequestLine{}, fmt.Errorf("request line must have exactly 3 parts")
 	}
 
 	method := parts[0]
 	if method != strings.ToUpper(method) {
-		return &RequestLine{}, fmt.Errorf("method must be uppercase")
+		return -1, &RequestLine{}, fmt.Errorf("method must be uppercase")
 	}
 
 	httpVersion := strings.Split(parts[2], "/")[1]
 	if httpVersion != "1.1" {
-		return &RequestLine{}, fmt.Errorf("http version must be 1.1")
+		return -1, &RequestLine{}, fmt.Errorf("http version must be 1.1")
 	}
 
-	return &RequestLine{
+	bytesRead := len(line) + 2 // +2 for the \r\n at the end
+
+	return bytesRead, &RequestLine{
 		HttpVersion:   httpVersion,
 		RequestTarget: parts[1],
 		Method:        method,
